9f76b301c6b4f238341d786ea577316dd39604cb
Rafael Fodra Furlan <rafael_viking_code@outlook.com>
2021-04-04 23:39:10 -0300

    basic window in Object Oriented Programming with Qt6

--- CalculadoraElemsMaqs/CalculadoraElemsMaqs.py
+++ CalculadoraElemsMaqs/CalculadoraElemsMaqs.py
@@ -1,1 +1,14 @@
+import sys
+import PyQt6
+from PyQt6 import QtWidgets, QtGui, QtCore, QtQuick
+from PyQt6.QtWidgets import QApplication, QWidget           #imports all we need for now
 
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()                                  #here we are extending the QWidget class, with the created class, wich makes everything easier
+
+app = QApplication([])                                      #Create a variable wich contains the QApplication built-in QWidgets
+janela = Window()                                           #create a variable wich contains the window function built-in QWidgets
+janela.show()                                               #now we show what we've created with the .function() thanks to object oriented programming
+
+sys.exit(app.exec())                                        #instatiate the loop
