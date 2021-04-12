8ba5fd5739a27a45f7ebe6c7c4f88a94bae53a37
Rafael Fodra Furlan <rafael_viking_code@outlook.com>
2021-04-12 16:57:47 -0300

    not good with QGraphicsItemAnimation

--- CalculadoraElemsMaqs/CalculadoraElemsMaqs.py
+++ CalculadoraElemsMaqs/CalculadoraElemsMaqs.py
@@ -18,7 +18,7 @@ class Window(QWidget):
         #space for function calls
         self.MWSkeleton()
         self.MWSceneanView()
-        
+        self.LinhaofTempo()
 
         self.show()
 
@@ -43,6 +43,7 @@ class Window(QWidget):
         self.pixmap1 = QGraphicsPixmapItem(QPixmap("gears-icon-vector"))
         self.pixmap1.setFlag(QGraphicsItem.ItemIsMovable)
         self.pixmap1.setScale(0.30)
+        self.pixmap1.toGraphicsObject()
 
         self.CenaMain.addItem(self.pixmap1)
         #logic test:
@@ -55,6 +56,25 @@ class Window(QWidget):
 ############################AnimationSection#################################
 
 
+    def LinhaofTempo (self):
+
+        self.linha = QTimeLine(1000)
+        self.linha.setFrameRange(0, 1000)
+        self.linha.setLoopCount(0)
+        def reloginho (self):
+            self.timer = QTimer()
+            self.timer.isSingleShot()
+        reloginho(self)
+        def Anime (self):
+            self.animenation = QGraphicsItemAnimation(self)
+            self.animenation.setTimeLine(self.linha)
+            self.animenation.setItem(self.pixmap1)
+            self.animenation.setPosAt(0.0, QPointF(0.0, 0.0))
+            self.animenation.setRotationAt(0.1, 30)
+            
+        Anime(self)
+        self.linha.start()
+        
 
 
 ############################Ending###########################################
