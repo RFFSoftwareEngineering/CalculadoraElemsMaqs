73f7614c774e2a906475bbd851c54b383dcb4a4f
Rafael Fodra Furlan <rafael_viking_code@outlook.com>
2021-04-12 09:00:05 -0300

    Main Window First Skeleton Complete

--- CalculadoraElemsMaqs/CalculadoraElemsMaqs.py
+++ CalculadoraElemsMaqs/CalculadoraElemsMaqs.py
@@ -5,19 +5,41 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
 from PySide6.QtWidgets import QGraphicsItem
-#clean restart
+
 class Window(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Calculadora Elementos de Máquinas")
-        self.setGeometry(150, 100, 1280, 720)
+        self.setWindowTitle("Calculadora Elementos de Máquinas v1.0")
+        self.setGeometry(150, 75, 1280, 720)
         self.setWindowIcon(QIcon("gears-icon-vector"))
+        self.setStyleSheet("background-color: rgb(51, 51, 53)")
         #space for function calls
 
+
+        self.MWSkeleton()
         self.show()
-#space to def new functions, objective == clean and functional code
-#clean restart
+
+
+#########################################################################
+    def MWSkeleton (self):
+
+        self.LabelTtl = QLabel("Calculadora de Elementos de Máquinas", self)
+        self.LabelTtl.setFont(QFont("Arial", 20, 8, True))
+        self.LabelTtl.move(350, 10)
+        self.LabelTtl.setStyleSheet("color: rgb(17, 11, 26)")
+        self.LabelInfo1 = QLabel("Escolha o Elemento:", self)
+        self.LabelInfo1.setFont(QFont("Arial", 16, 2, True))
+        self.LabelInfo1.setStyleSheet("color: rgb(17, 11, 26)")
+        self.LabelInfo1.move(900,42)
+        
+        
+
+
+
+
+
+
 app = QApplication([])
 janela = Window()
 sys.exit(app.exec_())
